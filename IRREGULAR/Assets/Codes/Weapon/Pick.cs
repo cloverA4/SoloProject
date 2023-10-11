7466c8df0984c626c0f51eddb8afde3cc1ab3ee1
RMS98 <urin4444@gamil.com>
2023-10-12 07:33:14 +0900

    엘리트 몬스터 애니메이션 수정 , 곡괭이 무기 구현중

---
+++ IRREGULAR/Assets/Codes/Weapon/Pick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick : MonoBehaviour
+{
+    [SerializeField] float _damage;
+    [SerializeField] float _per;
+
+    public void Init(float damage, int per)
+    {
+        this._damage = damage; //this 해당클래스에 접근
+        this._per = per;
+    }
+}
